db9a365f9a02fa57c527807b2219b24276815f65
Csillag61 <csillagok@gmail.com>
2025-07-26 17:58:15 -0400

    fixed linting issues

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 import os
 import typing
 
+
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> 'CleanUpFile':
         return self
 
     def __exit__(
-            self, 
-            exc_type: typing.Optional[type[BaseException]], 
-            exc_value: typing.Optional[BaseException], 
+            self,
+            exc_type: typing.Optional[type[BaseException]],
+            exc_value: typing.Optional[BaseException],
             traceback: typing.Optional[object]) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
