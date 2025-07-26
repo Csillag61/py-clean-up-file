b6566678e58391b77298984ff772f2ae304e741a
Csillag61 <csillagok@gmail.com>
2025-07-26 18:00:05 -0400

    fixed double quote

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> 'CleanUpFile':
+    def __enter__(self) -> "CleanUpFile":
         return self
 
     def __exit__(
